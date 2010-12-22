f8e04e46e136d7649a521210ee768fd24bd99d1c
Ben Jackman <benjamin.jackman@gmail.com>
2010-12-22 16:03:05 -0600

    initial commit
    Signed-off-by: Ben Jackman <benjamin.jackman@gmail.com>

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clojide "0.1.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
---
+++ test/clojide/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojide.test.core
+  (:use [clojide.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
